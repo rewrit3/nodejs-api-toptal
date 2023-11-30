5f4febf9c6e8cb3b65d8c49d6f7f11afa414ef1a
Adolfo <adolfo@rewrite.com.br>
2023-11-29 21:02:33 -0400

    refactor: UsersDao implementing mongoose

--- users/dao/users.dao.ts
+++ users/dao/users.dao.ts
@@ -1,6 +1,8 @@
 import shortid from 'shortid';
 import debug from 'debug';
 
+import mongooseService from '../../common/services/mongoose.service';
+
 import { CreateUserDto } from '../dto/create.user.dto';
 import { PatchUserDto } from '../dto/patch.user.dto';
 import { PutUserDto } from '../dto/put.user.dto';
@@ -8,84 +10,21 @@ import { PutUserDto } from '../dto/put.user.dto';
 const log: debug.IDebugger = debug('app:in-memory-dao');
 
 class UsersDao {
-  users: Array<CreateUserDto> = [];
+  Schema = mongooseService.getMongoose().Schema;
+
+  userSchema = new this.Schema({
+    _id: String,
+    email: String,
+    password: { type: String, select: false },
+    firstName: String,
+    lastName: String,
+    permissionFlags: Number,
+  }, { id: false });
+
+  User = mongooseService.getMongoose().model('Users', this.userSchema);
 
   constructor() {
     log('Created new instance of UsersDao');
-  }
-
-  async addUser(user: CreateUserDto) {
-    user.id = shortid.generate();
-
-    this.users.push(user);
-
-    return user.id;
-  }
-
-  async getUsers() {
-    return this.users;
-  }
-
-  async getUserById(userId: string) {
-    return this.users.find((user: { id: string}) => user.id === userId);
-  }
-
-  async putUserById(userId: string, user: PutUserDto) {
-    const objIndex = this.users.findIndex(
-      (obj: { id: string }) => obj.id === user.id
-    );
-
-    this.users.splice(objIndex, 1, user);
-
-    return `${user.id} updated via PUT`;
-  }
-
-  async patchUserById(userId: string, user: PatchUserDto) {
-    const objIndex = this.users.findIndex(
-      (obj: { id: string }) => obj.id === userId
-    );
-
-    let currentUser = this.users[objIndex];
-    const allowedPatchFields = [
-      'password',
-      'firstName',
-      'lastName',
-      'permissionLevel',
-    ];
-
-    for (let field of allowedPatchFields) {
-      if (field in user) {
-        // @ts-ignore
-        currentUser[field] = user[field];
-      }
-    }
-
-    this.users.splice(objIndex, 1, currentUser);
-
-    return `${user.id} patched`;
-  }
-
-  async removeUserById(userId: string) {
-    const objIndex = this.users.findIndex(
-      (obj: { id: string }) => obj.id === userId
-    );
-
-    this.users.splice(objIndex, 1);
-
-    return `${userId} removed`;
-  }
-
-  async getUserByEmail(email: string) {
-    const objIndex = this.users.findIndex(
-      (obj: { email: string }) => obj.email === email
-    );
-
-    let currentUser = this.users[objIndex];
-    if (currentUser) {
-      return currentUser;
-    } else {
-      return null;
-    }
   }
 }
 
